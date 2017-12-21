aa34adc2cbb5f6de47c92975d4168e233d19e027
группы стационара <st@itstep.lan>
2017-12-21 21:25:52 +0300

    fifth commit

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var reg = require('./routes/reg');
+
+var checkAuth = require('./utils/checkAuth');
 
 var app = express();
 
@@ -16,17 +19,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/reg',reg);
 app.use('/', index);
-app.use('/users', users);
+app.use('/users',checkAuth, users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +51,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(function (req, res, next) {
+   res.locals = {
+     userid: req.session.user
+   };
+   next();
+});
+
 
 app.listen(8129);
 module.exports = app;
--- routes/reg.js
+++ routes/reg.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Themas = require('../models/themas').Themas;
+
+//app.get('/logout', checkAuth, reg.logout);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('reg');	
@@ -8,7 +11,21 @@ router.get('/', function(req, res, next) {
  
 router.post('/',function(req,res,next){
 	var post = req.body;
-	console.log(post)
+	var Users = require('../models/users').Users;
+	var username = req.body.username;
+	var password = req.body.password;
+	var users = new Users({
+		username: username,
+		password: password
+	});
+	users.save(function(err, user){
+		console.log(arguments);
+		req.session.user = user._id;
+	}); 
+
 });
 
 module.exports = router;
+
+
+
